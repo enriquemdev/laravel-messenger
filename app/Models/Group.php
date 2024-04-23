442da062540a844b087218e12d2a3309e4aebe2a
Enrique <enriquemunozdev@gmail.com>
2024-04-23 17:58:49 -0600

    started the models

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Group extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'owner_id',
+        'last_message_id'
+    ];
+
+    public function users(): BelongsToMany {
+        return $this->belongsToMany(User::class, 'group_users');
+    }
 }
