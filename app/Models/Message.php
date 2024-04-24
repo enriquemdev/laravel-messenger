9005347382f2fab6a754b1516de5e5d902b36dab
Enrique <enriquemunozdev@gmail.com>
2024-04-24 14:42:24 -0600

    finished models

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Message extends Model
 {
@@ -15,4 +17,19 @@ class Message extends Model
         'group_id',
         'receiver_id',
     ];
+    public function sender(): BelongsTo {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    public function group(): BelongsTo {
+        return $this->belongsTo(Group::class);
+    }
+
+    public function attachments(): HasMany {
+        return $this->hasMany(MessageAttachment::class);
+    }
 }
